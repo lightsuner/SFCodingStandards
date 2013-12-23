b0174d47492d9ec1ad86e346f134cd6a3c59a23a
lightsuner <lightsuner@gmail.com>
2013-12-23 14:30:08 +0300

    add base standard

---
+++ Tests/Symfony2/AllTests.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * User: Dred
+ * Date: 23.12.13
+ * Time: 13:26
+ */
+
+
+class AllTests
+{
+
+    public function args_coma_delimeter($arg1,$arg2)
+    {
+        if (1==3) {
+
+        }
+
+        $arg_name = 'dd';
+        $argName = 'dd';
+
+        if (true) echo 'sdf';
+
+        new \DateTime;
+
+        $array = array(
+            'gg' => 'bb',
+            'gg1' => 'bb',
+            'gg2' => 'bb',
+            'gg3' => 'bb'
+        );
+
+        $array = array(
+            'newItem' => 'bb',
+            'new_item' => 'bb',
+        );
+
+        $gg = 'gg';
+        $fdf = 'dfd';
+
+        throw new \Exception('blablabla'.$gg."sdf $fdf");
+
+        return 'dd';
+    }
+
+    private $gg = 8;
+    public $ff = 2;
+
+}
+
+/**
+ * Class AllTestsw
+ *
+ * @package gfdg
+ * @subpackage dsfds
+ */
+class AllTestsw
+{
+}
+
+
+function get_items()
+{
+
+}
